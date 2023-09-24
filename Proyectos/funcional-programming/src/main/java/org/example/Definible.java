66bfae2a51ac48191ce8c832f90ae344eafd4f1c
germanpistan <anibalgermanpistan@gmail.com>
2023-09-24 09:45:58 -0300

    progra funcional

--- Proyectos/funcional-programming/src/main/java/org/example/Definible.java
+++ Proyectos/funcional-programming/src/main/java/org/example/Definible.java
@@ -1,8 +1,8 @@
 package org.example;
 
+@FunctionalInterface
 public interface Definible {
     //se puede definir la interface funcional fuera de la clase de trabajo
-    interface definible {
+    String definir();
 
-    }
 }
